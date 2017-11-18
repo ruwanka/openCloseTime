d6b63e0a34963f26d56ba93708fa858f2fc6c0f0
Ruwanka <ruwankamadhushan@gmail.com>
2017-11-18 18:06:15 +0530

    override toString method

--- library/src/main/java/com/ruwanka/openclosetime/Time.java
+++ library/src/main/java/com/ruwanka/openclosetime/Time.java
@@ -2,39 +2,49 @@ package com.ruwanka.openclosetime;
 
 public class Time {
 
-    private int hour;
+  private int hour;
 
-    private int minutes;
+  private int minutes;
 
-    private int seconds;
+  private int seconds;
 
-    public Time(int hour, int minutes, int seconds) {
-        this.hour = hour;
-        this.minutes = minutes;
-        this.seconds = seconds;
-    }
+  public Time(int hour, int minutes, int seconds) {
+    this.hour = hour;
+    this.minutes = minutes;
+    this.seconds = seconds;
+  }
 
-    public int getHour() {
-        return hour;
-    }
+  public int getHour() {
+    return hour;
+  }
 
-    public void setHour(int hour) {
-        this.hour = hour;
-    }
+  public void setHour(int hour) {
+    this.hour = hour;
+  }
 
-    public int getMinutes() {
-        return minutes;
-    }
+  public int getMinutes() {
+    return minutes;
+  }
 
-    public void setMinutes(int minutes) {
-        this.minutes = minutes;
-    }
+  public void setMinutes(int minutes) {
+    this.minutes = minutes;
+  }
 
-    public int getSeconds() {
-        return seconds;
-    }
+  public int getSeconds() {
+    return seconds;
+  }
 
-    public void setSeconds(int seconds) {
-        this.seconds = seconds;
-    }
+  public void setSeconds(int seconds) {
+    this.seconds = seconds;
+  }
+
+
+  @Override
+  public String toString() {
+    return "{"
+        + "\"hour\":\"" + hour + "\""
+        + ", \"minutes\":\"" + minutes + "\""
+        + ", \"seconds\":\"" + seconds + "\""
+        + "}";
+  }
 }
