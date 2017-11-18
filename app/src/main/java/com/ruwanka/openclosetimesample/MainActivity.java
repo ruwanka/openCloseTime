8bae671f0942be250da5123cedf165e6b4d79039
ruwanka <ruwankamadhushan@gmail.com>
2017-11-18 19:32:58 +0530

    observe open close time view updates in sample app

--- app/src/main/java/com/ruwanka/openclosetimesample/MainActivity.java
+++ app/src/main/java/com/ruwanka/openclosetimesample/MainActivity.java
@@ -2,12 +2,21 @@ package com.ruwanka.openclosetimesample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import com.ruwanka.openclosetime.OpenCloseTimeView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  private final static String TAG = "MainActivity";
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    OpenCloseTimeView timeView = findViewById(R.id.openCloseTimeView);
+    timeView.observe()
+        .subscribe(openCloseTime -> Log.d(TAG, openCloseTime.toString()));
+
+  }
 }
